6ddaa19b2f731e42ce75d74f5f4b7272bea63185
HyeonWooJeong <qhsh9713@gmail.com>
2021-05-09 22:35:06 +0900

    add swiftui's official tutorial (landmark app)

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Hyeon Woo Jeong on 2021/05/09.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
