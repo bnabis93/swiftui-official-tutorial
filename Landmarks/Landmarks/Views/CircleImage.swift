c212a888f7cc7af9621a9d3865ca773a4d874a10
HyeonWooJeong <qhsh9713@gmail.com>
2021-05-11 00:00:34 +0900

    Complete Building Lists and Navigation project

--- Landmarks/Landmarks/Views/CircleImage.swift
+++ Landmarks/Landmarks/Views/CircleImage.swift
@@ -8,16 +8,18 @@
 import SwiftUI
 
 struct CircleImage: View {
+    var image : Image
+    
     var body: some View {
-        Image("turtlerock")
-            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/) // MARK : use as a image's mask
+        image
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
             .overlay(Circle().stroke(Color.white, lineWidth: 4))
-            .shadow(radius: 7 )
+            .shadow(radius: 7)
     }
 }
 
 struct CircleImage_Previews: PreviewProvider {
     static var previews: some View {
-        CircleImage()
+        CircleImage(image: Image("turtlerock"))
     }
 }
